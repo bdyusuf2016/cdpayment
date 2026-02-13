edab1bc9e66c3977460c81e9623cfae5227811d5
Md. Yusuf Ali <mjyusuf40@gmail.com>
2026-02-13 08:12:34 +0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "./", // Ensures assets are linked correctly on GitHub Pages
+  base: "", // Ensures assets are linked correctly on GitHub Pages
   build: {
     outDir: "dist",
     assetsDir: "assets",
