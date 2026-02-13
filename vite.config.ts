dd02ae36fc4f31b86b193f6898e1ad6e80786923
Md. Yusuf Ali <mjyusuf40@gmail.com>
2026-02-13 07:43:33 +0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './cadpayment', // Ensures assets are linked correctly on GitHub Pages
+  base: './cdpayment', // Ensures assets are linked correctly on GitHub Pages
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
